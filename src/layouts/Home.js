bbc151b06ab9c18635c61f7702db68d779889080
Dreamscape03 <anshikaanshika103@gmail.com>
2024-05-16 00:57:31 +0530

    Update homepage CSS and add content

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -93,14 +93,140 @@ export const Home = () => {
         </div>
         <div className={Style.subHeaderContainer}>
           <div className={Style.subHeaderText}>
-            A collection of easy-to-use utility tools that will make your life
-            easier. Online tools that will help you in your daily life like
-            Online Clipboard, GPA Calculator, CGPA Calculator, and many more.
-            These tools are free to use and can be accessed from anywhere in the
-            world. You can use these tools to save time and make your life
-            easier. These tools are designed to be simple and easy to use so
-            that anyone can use them. You can use these tools to save time and
-            be more productive.
+            Simplify Your Life with Easy Utility Tools! The ultimate collection
+            of user-friendly tools at your fingertips. Bid farewell to tedious
+            tasks and embrace efficiency like never before. Effortlessly Copy,
+            Paste, and Organize with the Online Clipboard. Say goodbye to losing
+            important snippets of text or struggling to transfer data between
+            devices. Unlock Academic Excellence with GPA and CGPA Calculators.
+            Seamlessly track your academic progress and stay ahead of the curve.
+            The Possibilities are Endless with Easy Utility Tools. From unit
+            converters to advanced calculators, we've got you covered.
+            Productivity on the Go, Anytime, Anywhere. Access these powerful
+            tools from any device, at your convenience. Experience the Perfect
+            Fusion of Simplicity and Functionality. Intuitive interfaces and
+            user-friendly designs, making your life a breeze. Embrace the Future
+            of Productivity with Easy Utility Tools. Upgrade your workflow and
+            unlock your true potential today!
+          </div>
+        </div>
+        <div className={Style.featureSection}>
+          <div className={Style.featureHeader}>
+            Unveil the Power of Productivity with Easy Utility Tools' Remarkable
+            Features:
+          </div>
+          <div className={Style.featureContainer}>
+            <div className={Style.featureCard}>
+              <div className={Style.featureCardContent}>
+                <h5>Online Clipboard Features:</h5>
+                <ol style={{ marginBottom: "20px", paddingLeft: "20px" }}>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>1.</span> Store
+                    unlimited text snippets online.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>2.</span> Access your
+                    clipboard from any device.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>3.</span> Organize and
+                    categorize entries seamlessly.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>4.</span> Share
+                    specific text with collaborators
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>5.</span> Enjoy robust
+                    security and privacy
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>6.</span> Sync data
+                    with ease across devices and platforms.
+                  </li>
+                </ol>
+                <p>
+                  Simplify your workflow and enhance productivity with the Online!
+                </p>
+              </div>
+            </div>
+            <div className={Style.featureCard}>
+              <div className={Style.featureCardContent}>
+                <h5>GPA Calculator Features:</h5>
+                <ol style={{ marginBottom: "20px", paddingLeft: "20px" }}>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>1.</span> Input course
+                    details and grades effortlessly.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>2.</span> Calculate
+                    weighted GPAs for varying credit hours.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>3.</span> Get
+                    comprehensive GPA results and course breakdowns.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>4.</span> Track
+                    academic performance across semesters.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>5.</span> Set target
+                    GPAs and receive guidance
+                  </li>
+                </ol>
+                <p>Unlock your academic potential with ease!</p>
+              </div>
+            </div>
+            <div className={Style.featureCard}>
+              <div className={Style.featureCardContent}>
+                <h5>CGPA Calculator Features:</h5>
+                <ol style={{ marginBottom: "20px", paddingLeft: "20px" }}>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>1.</span> Input GPA and
+                    credits for multiple semesters.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>2.</span> Calculate
+                    accurate cumulative GPAs effortlessly.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>3.</span> Visualize
+                    academic progress with charts and graphs.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>4.</span> Set CGPA
+                    goals and track your trajectory.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>5.</span> Customize
+                    semester weightages as per your needs.
+                  </li>
+                  <li style={{ marginBottom: "10px" }}>
+                    <span style={{ fontWeight: "bold" }}>6.</span> Get
+                    personalized recommendations for academic planning.
+                  </li>
+                </ol>
+                <p>
+                  Elevate your academic journey with comprehensive CGPA
+                  insights!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={Style.feedbackSection}>
+          <div className={Style.feedbackHeader}>
+            The Easy Utility Tools Used and Loved by Millions Worldwide!
+          </div>
+          <div className={Style.feedbackContainer}>
+            <div className={Style.feedbackCard}>
+              <div className={Style.feedbackCardContent}>
+                <div className={Style.feedbackText}>
+                  "Easy Utility tools is your one-stop solution for storing and managing text snippets online. Enjoy all the tools you need to stay organized and efficient with your work while on the go!"
+                  </div>
+                </div>
+              </div>
           </div>
         </div>
       </div>
